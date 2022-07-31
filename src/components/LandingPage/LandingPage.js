5a3edcac750bd9e12615ca16862e79e3f606b350
saranya <dentist.apassionateprogrammer@gmail.com>
2022-07-31 10:36:05 +0530

    added landing page intro card

--- src/components/LandingPage/LandingPage.js
+++ src/components/LandingPage/LandingPage.js
@@ -7,12 +7,25 @@ function LandingPage() {
         <div className={classes.headerLeftBrandName}>
           <div className={classes.companyBrandLogo}></div> <h3> RecuitEasy</h3>
         </div>
-
         <div className={classes.headerRightButtons}>
           <button className={classes.findJobsButton}>Find jobs</button>
           <button className={classes.hireTalentButton}>Hire talent</button>
         </div>
       </header>
+      <main>
+        <div className={classes.landinPageIntroCard}>
+          <h3>Where techies realise their true potential</h3>
+          <p>
+            Look beyond the obvious. Use RecruitEasy to easily get discovered by
+            awesome<br></br>companies and get referred to job positions very few
+            know about it
+          </p>
+          <div className={classes.mainIntroButtons}>
+            <button className={classes.findJobsButton}>Find jobs</button>
+            <button className={classes.hireTalentButton}>Hire talent</button>
+          </div>
+        </div>
+      </main>
     </div>
   );
 }

ecfd6f38ac06a83c1180c4b5aef41e7aa5685906
saranya <dentist.apassionateprogrammer@gmail.com>
2022-07-31 18:27:27 +0530

    added routes

--- src/App.js
+++ src/App.js
@@ -3,14 +3,21 @@ import JobRegister from "./components/JobRegiter/JobRegister";
 import LandingPage from "./components/LandingPage/LandingPage";
 import Login from "./components/LoginPage/Login";
 import SignUp from "./components/SignupPage/SignUp";
-
+import { Routes, Route } from "react-router";
 function App() {
   return (
     <div className={classes.App}>
-      <LandingPage></LandingPage>
-      {/* <SignUp></SignUp> */}
-      {/* <Login></Login> */}
-      {/* <JobRegister></JobRegister> */}
+      <Routes>
+        <Route path="/" element={<LandingPage></LandingPage>}></Route>
+
+        <Route path="/Login" element={<Login></Login>}></Route>
+
+        <Route path="/Signup" element={<SignUp></SignUp>}></Route>
+        <Route
+          path="/registerWork"
+          element={<JobRegister></JobRegister>}
+        ></Route>
+      </Routes>
     </div>
   );
 }
